1462ca561e231063d77c99ce9755943f17f14768
Danail Branekov <danailster@gmail.com>
2020-02-26 16:59:06 +0000

    Creating process dirs is now possible in rootless
    [#168988942]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- process_test.go
+++ process_test.go
@@ -235,10 +235,6 @@ var _ = Describe("Process", func() {
 			})
 
 			Context("when working directory does not exist", func() {
-				BeforeEach(func() {
-					skipIfRootless()
-				})
-
 				It("spawns the process", func() {
 					stdout := runForStdout(container, garden.ProcessSpec{
 						User: "alice",
@@ -287,10 +283,6 @@ var _ = Describe("Process", func() {
 			})
 
 			Context("when working directory does not exist", func() {
-				BeforeEach(func() {
-					skipIfRootless()
-				})
-
 				It("should create the working directory, and succeed", func() {
 					exitCode, _, _ := runProcess(container, garden.ProcessSpec{
 						User: "alice",
